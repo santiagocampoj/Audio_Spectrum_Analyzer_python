6c12b8bbbd27aeb2234878bd5bae612476b40050
Santiago Campo <id00706137@usal.es>
2023-11-23 23:03:36 +0100

    Add logger and update constants

--- constants.py
+++ constants.py
@@ -1,5 +1,19 @@
+import pyaudio
+from logger_info import setup_logging
+logger = setup_logging()
+
 # Constants
 CHUNK = 1024 * 4  # Samples per frame = 4096
 FORMAT = pyaudio.paInt16  # Audio format (bytes per sample)
 CHANNELS = 1  # Single channel for microphone
 RATE = 44100  # Samples per second
+
+logger.info(f"""
+            Constants:
+
+            CHUNK \t{CHUNK}
+            FORMAT \t{FORMAT}
+            CHANNELS \t{CHANNELS}
+            RATE \t{RATE}
+            
+            """)
